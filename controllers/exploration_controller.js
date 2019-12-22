010d1ac0c674a70d6ac63b21da67274cf64bae63
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-12-22 11:36:19 -0600

    Add function to filter by clinic

--- controllers/exploration_controller.js
+++ controllers/exploration_controller.js
@@ -26,8 +26,29 @@ const findExplorationByBooking = (req, res) => {
     })
 }
 
+const filterByClinic = (req, res) => {
+  const { clinic, limit, page } = req.query;
+  Exploration.find({})
+    .lean()
+    .limit(parseInt(limit))
+    .skip(parseInt(page * limit))
+    .populate({
+      path: 'bookingId',
+      match: {
+        'clinicName': clinic
+      }
+    })
+    .exec((err, explorations) => {
+      explorations = explorations.filter(exploration => exploration['bookingId'] !== null)
+      if (err) return res.status(500).send({ code: 500, message: "Something went wrong" });
+      if (!explorations) return res.status(404).send({ code: 404, message: "Theres not explorations" });
+      return res.status(200).send({ code: 200, data: explorations, message: "Request successfully" });
+    })
+}
+
 
 module.exports = {
   index,
-  findExplorationByBooking
+  findExplorationByBooking,
+  filterByClinic
 }
--- routes/exploration_routes.js
+++ routes/exploration_routes.js
@@ -4,6 +4,7 @@ const exploration_controller = require('../controllers/exploration_controller');
 
 route.get('/explorations', exploration_controller.index);
 route.get('/exploration/booking/:id', exploration_controller.findExplorationByBooking);
+route.get('/explorations/search', exploration_controller.filterByClinic);
 
 module.exports = route;
 
