b6b7442d22805fcb986c81370f14f1ea21322cc9
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-12-22 19:26:29 -0600

    Install supertest, mocha and add some test for explorations and bookings routes

---
+++ tests/test.js
@@ -0,0 +1,32 @@
+var request = require('supertest')
+var request = request("http://localhost:5000/api/v1")
+
+describe('Explorations', () => {
+  describe('GET', () => {
+    it('Should return json with explorations', done => {
+      request.get('/explorations/?page=0&limit=10')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+  });
+});
+
+describe('Explorations association', () => {
+  describe('GET', () => {
+    it('Should return json with explorations and bookings', done => {
+      request.get('/explorations/booking/12345a')
+        .expect(404, done);
+    });
+  });
+});
+
+
+describe('Bookings', () => {
+  describe('GET', () => {
+    it('Should return json with bookings', done => {
+      request.get('/bookings/?page=0&limit=10')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+  });
+});
